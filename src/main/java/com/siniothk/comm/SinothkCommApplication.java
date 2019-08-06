bbfc9bebf718ba1c7377e17cfe7e0b90ffeaf5e4
LiangYT <381518188@qq.com>
2019-08-06 11:03:24 +0800

    初始化！

---
+++ src/main/java/com/siniothk/comm/SinothkCommApplication.java
@@ -0,0 +1,13 @@
+package com.siniothk.comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinothkCommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinothkCommApplication.class, args);
+    }
+
+}
